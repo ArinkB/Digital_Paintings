c9c0ae6acfde0700f5ca27fc63a85f91e6f7e91d
Arink <arinkbertrand@gmail.com>
2020-07-13 14:00:30 -0400

    created function to add text to bottom center of images

--- paintings.py
+++ paintings.py
@@ -1,12 +1,40 @@
 import os
-from PIL import Image, ImageOps
+from PIL import Image, ImageOps, ImageDraw, ImageFont
 import glob
 
-img = Image.open('Art/AB-2020(2).jpg')
+
+## SINGLE IMAGE
+
+img = Image.open('Art/AB-2020 (2).jpg')
 add_grey_border = ImageOps.expand(img,border=50,fill='grey')
 add_black_border = ImageOps.expand(add_grey_border,border=300,fill='black')
-add_plaque = 
-add_black_border.save('imaged-with-border.png')
+add_black_border.save('Art/imaged-with-border.png')
+
+## Function to add Title to image
+
+def add_title(img_src, title):
+
+    img = Image.open(img_src, 'r')
+    draw = ImageDraw.Draw(img)
+    w, h = img.size
+    
+
+    font = ImageFont.truetype("arial.ttf", 100)
+    text_w, text_h = draw.textsize(title, font)
+
+    draw.text(((w - text_w) / 2, (h-100) - text_h), title, (255,255,255), font=font, stroke_width=2, stroke_fill='#eeda19')
+
+    img.save(img_src)
+
+    return img_src
+
+add_title('Art/imaged-with-border.png', 'test')
+
+
+
+
+
+## Loop through all images 
 
 # os.chdir('Art')
 
