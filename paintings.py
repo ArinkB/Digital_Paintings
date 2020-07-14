53818e4642e858f644e8f433d302b3293a699f20
Arink <arinkbertrand@gmail.com>
2020-07-14 16:21:14 -0400

    add title based on file name

--- paintings.py
+++ paintings.py
@@ -3,12 +3,23 @@ from PIL import Image, ImageOps, ImageDraw, ImageFont
 import glob
 
 
-## SINGLE IMAGE
+# ## SINGLE IMAGE
 
-img = Image.open('Art/AB-2020 (2).jpg')
-add_grey_border = ImageOps.expand(img,border=50,fill='grey')
-add_black_border = ImageOps.expand(add_grey_border,border=300,fill='black')
-add_black_border.save('Art/imaged-with-border.png')
+# img = Image.open('Art/AB-2020 (2).jpg')
+# add_grey_border = ImageOps.expand(img,border=50,fill='grey')
+# add_black_border = ImageOps.expand(add_grey_border,border=300,fill='black')
+# add_black_border.save('Art/imaged-with-border.png')
+
+os.chdir('Art')
+
+## Display correct Name
+for filename in glob.glob('*.jpg'):
+    if filename.startswith('AB'):
+        title = "Aveen"
+    elif filename.startswith('OB'):
+        title = "Olivia"  
+    elif filename.startswith('KB'):
+        title = "Katie"
 
 ## Function to add Title to image
 
@@ -24,21 +35,14 @@ def add_title(img_src, title):
     draw.text(((w - text_w) / 2, (h-100) - text_h), title, (255,255,255), font=font, stroke_width=2, stroke_fill='#eeda19')
     img.save(img_src)
     return img_src
-
-add_title('Art/imaged-with-border.png', 'test')
-
-
-
-
-
+             
 ## Loop through all images 
 
-# os.chdir('Art')
-
-# for i in glob.glob('*.jpg'):
-#   img = Image.open(i)
-#   add_grey_border = ImageOps.expand(img,border=50,fill='grey')
-#   add_black_border = ImageOps.expand(add_grey_border,border=300,fill='black')
-#   add_black_border.save('%s' % i)
+for i in glob.glob('*.jpg'):
+  img = Image.open(i)
+  add_grey_border = ImageOps.expand(img,border=50,fill='grey')
+  add_black_border = ImageOps.expand(add_grey_border,border=300,fill='black')
+  add_black_border.save('%s' % i)
+  add_title(i, title)
 
 
